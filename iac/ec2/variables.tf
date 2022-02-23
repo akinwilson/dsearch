eb56a888b14b0deaf14561577e5fe697a97886c1
akinwilson <akinola.antony.wilson@gmail.com>
2022-02-23 09:39:36 +0000

    ec2 for checking efs

---
+++ iac/ec2/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  description = "Name of stack module"
+}
+
+variable "environment" {
+  description = "the name of your environment, e.g. 'production' or 'development'"
+}
+
+variable "vpc_id" {
+    description = "The vpc to launch the EC2 instance into"
+}
+
+variable "subnet_id"{
+    description = "The subnet (public) to launch the EC2 instance into"
+}
+
+variable "ami" {
+  description = "Image to be used with ec2 instance"
+  default =  "ami-0dd555eb7eb3b7c82"
+}
