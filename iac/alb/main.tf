21051d960c5c82cfe76dc703935f045422efb42a
akinwilson <akinola.antony.wilson@gmail.com>
2022-03-03 09:52:05 +0000

    Adding ssl certifcates to lb
    Every site that doesnt use ssl is labelled as insecure by most browser. Therefore the additional resource required have been configured here to force the lb to redirect from port 80 to port 443 and use https rather than http

--- iac/alb/main.tf
+++ iac/alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_lb" "main" {
 resource "aws_alb_target_group" "main" {
   name        = "${var.name}-tg-${var.environment}"
   port        = 7700
-  protocol    = "HTTP"
+  protocol    = "HTTPS"
   vpc_id      = var.vpc_id
   target_type = "ip"
 
@@ -36,17 +36,40 @@ resource "aws_alb_target_group" "main" {
   }
 }
 
-# http listener
-resource "aws_alb_listener" "http" {
+# https listener
+resource "aws_alb_listener" "https" {
   load_balancer_arn = aws_lb.main.id
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  sssl_policy = "ELBSecurityPolicy-2016-08"
   default_action {
     target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
 }
 
+resource "aws_acm_certificate" "main" {
+  domain_name       = aws_lb.main.dns_name 
+  validation_method = "DNS"
+  tags = {
+    Name        = "${var.name}-ssl-cert-${var.environment}"
+    Environment = var.environment
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener_certificate" "main" {
+  listener_arn    = aws_alb_listener.https.arn
+  certificate_arn = aws_acm_certificate.main.arn
+}
+
+
+
+
 output "aws_alb_target_group_arn" {
   value = aws_alb_target_group.main.arn
 }
+
+
